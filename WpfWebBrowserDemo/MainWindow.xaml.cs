4bc9baf6bc589d6603010691ba3c54faadd8f0f3
Cielo Sun <CieloSun@outlook.com>
2017-07-27 14:22:37 +0800

    添加项目文件。

---
+++ WpfWebBrowserDemo/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WpfWebBrowserDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            webBrowser.Source = new Uri("http://www.cnblogs.com/cielosun");
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            string uri = textBox.Text;
+            if (!(uri.Contains("http://") || uri.Contains("https://")))
+            {
+                uri = "http://" + uri;
+                textBox.Text = uri;
+            }
+            webBrowser.Source = new Uri(uri);
+        }
+
+        private void WebBrowser_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void ButtonPrint_Click(object sender, RoutedEventArgs e)
+        {
+            webBrowser.Print();
+        }
+    }
+}
---
+++ WpfWebBrowserDemo/WebBrowserExtensions.cs
@@ -0,0 +1,42 @@
+﻿using mshtml;
+using System.Diagnostics;
+using System.Reflection;
+using System.Windows.Controls;
+
+namespace WpfWebBrowserDemo
+{
+    public static class WebBrowserExtensions
+    {
+        public static void ScriptErrorsSuppressed(this WebBrowser webBrowser, bool hide = true)
+        {
+            FieldInfo fiComWebBrowser = typeof(WebBrowser).GetField("_axIWebBrowser2", BindingFlags.Instance | BindingFlags.NonPublic);
+            if (fiComWebBrowser == null) return;
+
+            object objComWebBrowser = fiComWebBrowser.GetValue(webBrowser);
+            if (objComWebBrowser == null) return;
+
+            objComWebBrowser.GetType().InvokeMember("Silent", BindingFlags.SetProperty, null, objComWebBrowser, new object[] { hide });
+        }
+        public static void AddJavaScriptElement(this WebBrowser webBrowser, string javaScriptString)
+        {
+            if (webBrowser.Document is HTMLDocument doc)
+            {
+                var element = doc.createElement("script") as HTMLScriptElement;
+                element.type = "text/javascript";
+                element.text = javaScriptString;
+                var nodes = doc.getElementsByTagName("head");
+
+                foreach (var node in nodes)
+                {
+                    //Debug.WriteLine((node as HTMLHeadElement).lastChild.nodeName);
+                    (node as HTMLHeadElement).appendChild(element as IHTMLDOMNode);
+                    //Debug.WriteLine((node as HTMLHeadElement).lastChild.nodeName);
+                }
+            }
+        }
+        public static void Print(this WebBrowser webBrowser)
+        {
+            (webBrowser.Document as HTMLDocument).execCommand("Print");
+        }
+    }
+}
